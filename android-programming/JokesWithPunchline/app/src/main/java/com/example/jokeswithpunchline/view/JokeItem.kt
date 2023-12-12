1fc0f61ffb842078442bac1fafbb1c404e215fea
Koshak1432 <70834575+Koshak1432@users.noreply.github.com>
2023-12-12 23:34:25 +0700

    updated ui, add button to show/hide punch, seems done

--- android-programming/JokesWithPunchline/app/src/main/java/com/example/jokeswithpunchline/view/JokeItem.kt
+++ android-programming/JokesWithPunchline/app/src/main/java/com/example/jokeswithpunchline/view/JokeItem.kt
@@ -2,53 +2,82 @@ package com.example.jokeswithpunchline.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import com.example.jokeswithpunchline.model.Joke
 
 @Composable
 fun JokeItem(joke: Joke) {
+    var isPunchlineVisible by rememberSaveable { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .padding(8.dp, 4.dp)
-            .fillMaxWidth()
-            .background(Color.Green)
+            .fillMaxWidth(),
+        elevation = 8.dp
     ) {
-        Surface {
-            Row(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .background(Color.Magenta)
-            ) {
-                Surface(shape = MaterialTheme.shapes.medium) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row {
+                Text(
+                    text = joke.type,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Color(0xFF6DB9EF)),
+                    style = MaterialTheme.typography.body2
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = { isPunchlineVisible = !isPunchlineVisible },
+                    modifier = Modifier
+                        .height(30.dp)
+                        .padding(3.dp),
+                    contentPadding = PaddingValues(3.dp)
+                ) {
                     Text(
-                        text = joke.type,
-                        modifier = Modifier.padding(all = 4.dp),
-                        style = MaterialTheme.typography.body2
+                        text = if (isPunchlineVisible) "Hide punch" else "Show punch"
                     )
-                }
-                Column {
-                    Text(text = joke.setup)
-                    Column {
-                        Text(text = joke.punchline)
-                    }
+
                 }
             }
-
+            Spacer(modifier = Modifier.height(8.dp))
+            Column {
+                Text(
+                    text = joke.setup,
+                    style = MaterialTheme.typography.body1
+                )
+                if (isPunchlineVisible) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = joke.punchline,
+                        style = MaterialTheme.typography.subtitle1,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
         }
     }
 }
